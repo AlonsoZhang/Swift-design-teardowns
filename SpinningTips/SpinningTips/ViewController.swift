479db9d0d68e17e858d3d98fb51478cad856be19
Alonso <code@zhangwu.space>
2016-08-06 08:46:48 +0800

    Init SpinningTips project

---
+++ SpinningTips/SpinningTips/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SpinningTips
+//
+//  Created by Alonso Zhang on 16/8/6.
+//  Copyright © 2016年 Alonso Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
